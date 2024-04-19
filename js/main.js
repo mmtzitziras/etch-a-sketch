d27f2ea744df8ab54a7a225adb6c85b6ee6667b9
mmtzitziras <mariostzitziras4@gmail.com>
2024-04-19 16:29:56 +0300

    Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
-const container = document.querySelector('.container');
+let gridSize = 16;
 
-for (let i = 0; i < 256 + 16; i++) {
-    const div = document.createElement('div');
-    div.addEventListener("mouseover", event => {
-        div.style.backgroundColor = 'red';
-    });
-    div.addEventListener("mouseout", event => {
-        div.style.backgroundColor = 'white';
-    });
-    container.appendChild(div);
+const container = document.querySelector('.container');
+const body = document.querySelector('body');
+const changeGrid = document.querySelector('.change-grid'); 
+createGrid();   
+
+changeGrid.addEventListener('click', changeSize => {
+    let newSize = parseInt(prompt('Enter new grid size:'));
+    while (newSize<0){
+        let newSize = parseInt(prompt('Enter new grid size:'));
+    }
+    removeGrid();
+    gridSize = newSize;
+    createGrid();
+});
+
+
+function createGrid() {
+
+    for (let i = 0; i < gridSize*gridSize + gridSize; i++) {
+        const div = document.createElement('div');
+        div.addEventListener("mouseover", event => {
+            div.style.backgroundColor = 'red';
+        });
+        div.addEventListener("mouseout", event => {
+            div.style.backgroundColor = 'white';
+        });
+        if ( i % (gridSize + 1) === 0){
+            
+            div.style.height = "0";
+            div.style.border = "0";
+            div.style.marginRight = "100%";
+        }
+        container.appendChild(div);
+    }
+    body.style.minWidth = `${gridSize * 40 + gridSize*10}px`;
 }
+
+function removeGrid(){
+
+    container.replaceChildren();
+}
+
+
 
