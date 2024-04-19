1d02a4f8d4c5871906b3c2b94d1fa55487bb47b7
mmtzitziras <mariostzitziras4@gmail.com>
2024-04-19 17:24:11 +0300

    Fixed grid size and each box size so it fits inside the container

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 let gridSize = 16;
 
 const container = document.querySelector('.container');
-const body = document.querySelector('body');
 const changeGrid = document.querySelector('.change-grid'); 
 createGrid();   
 
 changeGrid.addEventListener('click', changeSize => {
     let newSize = parseInt(prompt('Enter new grid size:'));
-    while (newSize<0){
-        let newSize = parseInt(prompt('Enter new grid size:'));
+    while (newSize<0 || newSize>100){
+        newSize = parseInt(prompt('Enter new grid size:'));
     }
     removeGrid();
     gridSize = newSize;
@@ -18,7 +17,7 @@ changeGrid.addEventListener('click', changeSize => {
 
 function createGrid() {
 
-    for (let i = 0; i < gridSize*gridSize + gridSize; i++) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
         const div = document.createElement('div');
         div.addEventListener("mouseover", event => {
             div.style.backgroundColor = 'red';
@@ -26,15 +25,18 @@ function createGrid() {
         div.addEventListener("mouseout", event => {
             div.style.backgroundColor = 'white';
         });
-        if ( i % (gridSize + 1) === 0){
+
+        div.style.width = `${(500/gridSize) - 2}px`;
+        div.style.height = `${(500/gridSize) - 2}px`;
+        // if ( i % (gridSize + 1) === 0){
             
-            div.style.height = "0";
-            div.style.border = "0";
-            div.style.marginRight = "100%";
-        }
+        //     div.style.height = "0";
+        //     div.style.border = "0";
+        //     div.style.marginRight = "100%";
+        // }
         container.appendChild(div);
     }
-    body.style.minWidth = `${gridSize * 40 + gridSize*10}px`;
+    
 }
 
 function removeGrid(){
